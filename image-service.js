8eae00447aacca47594780ca8f45d8819e71d20a
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-08-30 00:51:57 +0200

    create axios instance

---
+++ image-service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: `http://localhost:3000`,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  fetchImages() {
+    return apiClient.get("api/images");
+  },
+  uploadImage(file) {
+    const formData = new FormData();
+    formData.append("image", file);
+    return apiClient.post("api/image", formData);
+  },
+};
