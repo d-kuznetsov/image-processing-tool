efd1d0b19abf9b19d4fd41a13d9df544ea8ff2e4
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-09-11 15:48:35 +0200

    use serve-handler middleware

--- src/pages/api/image/[...params].js
+++ src/pages/api/image/[...params].js
@@ -1,18 +1,33 @@
+import { join } from "path";
+import serveHandler from "serve-handler";
+import { UPLOADED_DIR } from "../../../config";
+
+const API_PREFIX = "/api/image";
+
+export default async (req, res) => {
+  req.url = req.url.replace(API_PREFIX, "");
+  await serveHandler(req, res, {
+    public: join(process.cwd(), UPLOADED_DIR),
+  });
+};
+
+/*
 import fs from "fs";
 import path from "path";
-import { UPLOADED_DIR } from '../../../config'
+import { UPLOADED_DIR } from "../../../config";
 
 export default (req, res) => {
-    const [size, filename] = req.query.params;
-    const filePath = path.join(process.cwd(), UPLOADED_DIR, size, filename);
-    if (!fs.existsSync(filePath)) {
-        console.log(filePath + 'not found');
-        res.writeHead(404, {'Content-Type': 'text/plain'})
-        res.write('404 Not Found');
-        res.end();
-        return;
-      }
-    res.writeHead(200, { 'Content-Type': 'image/*' });
-    const fileStream = fs.createReadStream(filePath);
-    fileStream.pipe(res);
-}
+  const [size, filename] = req.query.params;
+  const filePath = path.join(process.cwd(), UPLOADED_DIR, size, filename);
+  if (!fs.existsSync(filePath)) {
+    console.log(filePath + "not found");
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("404 Not Found");
+    res.end();
+    return;
+  }
+  res.writeHead(200, { "Content-Type": "image/*" });
+  const fileStream = fs.createReadStream(filePath);
+  fileStream.pipe(res);
+};
+*/
