acb1e4916a55eaa2871f6f20c33e2759529f9ed4
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-09-26 13:28:46 +0200

    use axios; replace http-server with node-static

--- dev env (issue #6)/src/index.js
+++ dev env (issue #6)/src/index.js
@@ -1,15 +1,16 @@
+import axios from "axios";
 import "./styles/base.scss";
 import logo from "./assets/logo.png";
+
+const BACKEND_URL = "http://localhost:3000/api/get/images?limit=3&order=name";
 
 document.querySelector("img").setAttribute("src", logo);
 
 (async function fetchImages() {
   try {
-    const res = await fetch(
-      "http://localhost:8080/api/get/images?limit=3&order=name"
-    );
-    const data = await res.json();
-    document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+    document.querySelector("pre").innerHTML = "Loading...";
+    const res = await axios.get(BACKEND_URL);
+    document.querySelector("pre").innerHTML = JSON.stringify(res.data, null, 2);
   } catch (err) {
     document.querySelector("pre").innerHTML = err;
   }
