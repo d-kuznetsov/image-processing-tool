70ed29876c2df1b33c75c22d94a002da379ae4e7
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2020-09-27 21:16:34 +0200

    add bg color, width, height to image

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { IMAGE_SIZE_TO_PREVIEW, FILE_EXT } from "../constants";
+import {
+  IMAGE_SIZE_TO_PREVIEW as IMG_SIZE,
+  FILE_EXT,
+  IMAGE_TYPES,
+} from "../constants";
 import { useAppContext } from "../context";
 import GridItemToolbar from "./GridItemToolbar";
+
+const { maxWidth: imgWidht, maxHeight: imgHeight } = IMAGE_TYPES[IMG_SIZE];
 
 export default function Grid({ initialItems }) {
   const { images: items } = useAppContext();
 
   return (
     <div className="flex flex-wrap justify-center">
-      {(items || initialItems).map(({ id }) => {
+      {(items || initialItems).map(({ id, name }) => {
         return (
           <article
             key={id}
-            className="group relative  flex-center p-4 border-solid border-2 border-transparent hover:border-blue-400 hover:bg-blue-100 rounded-md cursor-pointer"
+            className="group relative flex-center p-4 border-solid border-2 border-transparent hover:border-blue-400 hover:bg-blue-100 rounded-md cursor-pointer"
           >
             <img
-              className="rounded-md"
-              src={`api/get/image/${IMAGE_SIZE_TO_PREVIEW}/${id}${FILE_EXT}`}
+              className="rounded-md bg-blue-100"
+              src={`api/get/image/${IMG_SIZE}/${id}${FILE_EXT}`}
+              style={{ width: imgWidht, height: imgHeight }}
+              alt={name}
             />
             <GridItemToolbar id={id} />
           </article>
